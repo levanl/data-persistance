8255ff48707867d4b69e4c571e232cff5c52fda3
levanloladze <levan.loladzew@gmail.com>
2023-11-05 11:14:30 +0400

    first commit

---
+++ DataPersistance/DataPersistance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataPersistance
+//
+//  Created by Levan Loladze on 05.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
